5c2ec16f39ea16040f14d4832562f3cc4ed90295
Maksymilian Kowalski <makow.088@gmail.com>
2025-04-16 12:26:43 +0200

    Add `redundant_brackets_in_enum_call` fixer

--- tests/redundant_brackets_in_enum_call/mod.rs
+++ tests/redundant_brackets_in_enum_call/mod.rs
@@ -1,4 +1,4 @@
-use crate::test_lint_diagnostics;
+use crate::{test_lint_diagnostics, test_lint_fixer};
 
 const REDUNDANT_BRACKET_CALL: &str = r#"
 #[derive(Drop)]
@@ -89,4 +89,43 @@ fn allow_multiple_empty_variants_diagnostics() {
 #[test]
 fn tuple_variant_diagnostics() {
     test_lint_diagnostics!(TUPLE_VARIANT, @r"");
+}
+
+#[test]
+fn redundant_bracket_call_fixer() {
+    test_lint_fixer!(REDUNDANT_BRACKET_CALL, @r"
+    #[derive(Drop)]
+    enum MyEnum {
+        Data: u8,
+        Empty
+    }
+      
+    fn main() {
+        let _a = MyEnum::Empty; 
+    }
+    ");
+}
+
+#[test]
+fn multiple_empty_variants_fixer() {
+    test_lint_fixer!(MULTIPLE_REDUNDANT_BRACKETS, @r"
+    #[derive(Drop)]
+    enum MyEnum {
+        Data: u8,
+        Empty1,
+        Empty2, 
+        Empty3
+    }
+      
+    fn main() {
+        let _a = MyEnum::Empty1; // Comment
+        let _b = MyEnum::Empty2;
+        let _c = MyEnum::Empty3;
+    }
+    ");
+}
+
+#[test]
+fn allow_multiple_empty_variants_fixer() {
+    test_lint_diagnostics!(ALLOW_MULTIPLE_REDUNDANT_BRACKETS, @r"");
 }
