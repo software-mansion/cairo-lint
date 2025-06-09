33f7a3aaa41f412ea75218f6419b7d3e51a357a3
Maksymilian Kowalski <makow.088@gmail.com>
2025-06-09 17:09:30 +0200

    Add `API` function to get `fixes` without resolving overlaps

--- src/lib.rs
+++ src/lib.rs
@@ -69,6 +69,27 @@ pub fn get_fixes(
         .collect()
 }
 
+/// Gets all possible fixes for a set of compiler diagnostics (that uses Cairo lint analyzer plugin)
+/// without resolving overlapping fixes. This is needed when you want to see all potential fixes,
+/// even if they might conflict with each other.
+///
+/// # Arguments
+///
+/// * `db` - The reference to the `dyn SemanticGroup` that the diagnostics were based upon.
+/// * `diagnostics` - The list of compiler diagnostics. Make sure that the diagnostics from the Cairo lint analyzer plugin are also included.
+///
+/// # Returns
+///
+/// A HashMap where:
+/// * keys are FileIds (that points to a file that the fixes might be applied to)
+/// * values are vectors of proposed Fixes.
+pub fn get_separated_fixes(
+    db: &(dyn SemanticGroup + 'static),
+    diagnostics: Vec<SemanticDiagnostic>,
+) -> HashMap<FileId, Vec<Fix>> {
+    get_fixes_without_resolving_overlapping(db, diagnostics)
+}
+
 /// Applies the fixes to the file.
 ///
 /// # Arguments
