83c29b32e0802ba0e1af363e5de354492b6f63d8
Jan Smółka <jp.smolka@gmail.com>
2026-04-10 15:04:29 +0200

    Migrate to granular file inputs

--- src/fixer/db.rs
+++ src/fixer/db.rs
@@ -1,5 +1,9 @@
 use cairo_lang_defs::db::{defs_group_input, init_external_files};
-use cairo_lang_filesystem::db::files_group_input;
+use cairo_lang_filesystem::db::{
+    FileContentStorage, FileContentView, files_group_input, new_file_content_storage,
+    register_files_group_view, set_generated_file_content_for_input,
+    set_on_disk_file_content_for_input, snapshot_file_contents,
+};
 use cairo_lang_lowering::{db::init_lowering_group, optimizations::config::Optimizations};
 use cairo_lang_semantic::db::semantic_group_input;
 use salsa::{Database, Setter};
@@ -8,9 +12,15 @@ use salsa::{Database, Setter};
 #[derive(Clone)]
 pub struct FixerDatabase {
     storage: salsa::Storage<Self>,
+    file_contents: FileContentStorage,
 }
 
 impl salsa::Database for FixerDatabase {}
+impl FileContentView for FixerDatabase {
+    fn file_content_storage(&self) -> Option<&FileContentStorage> {
+        Some(&self.file_contents)
+    }
+}
 
 impl FixerDatabase {
     pub fn new_from(db: &dyn Database) -> Self {
@@ -57,9 +67,6 @@ impl FixerDatabase {
             .set_crate_configs(&mut new_db)
             .to(files_group_input(db).crate_configs(db).clone());
         files_group_input(&new_db)
-            .set_file_overrides(&mut new_db)
-            .to(files_group_input(db).file_overrides(db).clone());
-        files_group_input(&new_db)
             .set_flags(&mut new_db)
             .to(files_group_input(db).flags(db).clone());
         files_group_input(&new_db)
@@ -71,12 +78,24 @@ impl FixerDatabase {
         // that is never supposed to be changed after the initialization.
         init_external_files(&mut new_db);
 
+        for (file_input, (editor_content, generated_content)) in snapshot_file_contents(db) {
+            if editor_content.is_some() {
+                set_on_disk_file_content_for_input(&mut new_db, file_input.clone(), editor_content);
+            }
+            if generated_content.is_some() {
+                set_generated_file_content_for_input(&mut new_db, file_input, generated_content);
+            }
+        }
+
         new_db
     }
 
     fn new() -> Self {
-        Self {
+        let db = Self {
             storage: Default::default(),
-        }
+            file_contents: new_file_content_storage(),
+        };
+        register_files_group_view(&db);
+        db
     }
 }
